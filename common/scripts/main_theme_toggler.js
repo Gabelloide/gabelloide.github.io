55b1cf4e619911375e4a5c46997e0597c90a01dd
Gabelloide <olivier.gabelle1@gmail.com>
2023-04-06 23:40:50 +0200

    Added Light Theme
    This light theme is memorized between the pages of the website. It may need some improvements.

---
+++ common/scripts/main_theme_toggler.js
@@ -0,0 +1,23 @@
+function toggleTheme() {
+  var link = document.querySelector("link[id='theme-link']");
+  var isLight = link.getAttribute("href") == "common/css/light-style.css";
+
+  if (isLight) {
+    link.setAttribute("href", "common/css/style.css");
+    localStorage.setItem("theme", "dark");
+  } else {
+    link.setAttribute("href", "common/css/light-style.css");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var link = document.querySelector("link[id='theme-link']");
+  var theme = localStorage.getItem("theme");
+
+  if (theme == "light") {
+    link.setAttribute("href", "common/css/light-style.css");
+  } else {
+    link.setAttribute("href", "common/css/style.css");
+  }
+});
