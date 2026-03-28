bad9b67a3dc4a104c39342767d92d20b1a228f71
Olivier <olivier.gabelle1@gmail.com>
2026-03-28 21:43:21 +0100

    better light mode images

--- common/scripts/secondary_theme_toggler.js
+++ common/scripts/secondary_theme_toggler.js
@@ -1,3 +1,49 @@
+function getThemedImageSrc(src, isLight) {
+  if (isLight) {
+    return /_light\.png(\?.*)?$/i.test(src)
+      ? src
+      : src.replace(/\.png(\?.*)?$/i, "_light.png$1");
+  }
+
+  return src.replace(/_light\.png(\?.*)?$/i, ".png$1");
+}
+
+function switchTopImageSmooth(image, nextSrc) {
+  if (image.getAttribute("src") === nextSrc) {
+    return;
+  }
+
+  image.classList.add("is-switching");
+
+  var preloaded = new Image();
+  preloaded.onload = function () {
+    image.setAttribute("src", nextSrc);
+    requestAnimationFrame(function () {
+      image.classList.remove("is-switching");
+    });
+  };
+  preloaded.onerror = function () {
+    image.setAttribute("src", nextSrc);
+    image.classList.remove("is-switching");
+  };
+  preloaded.src = nextSrc;
+}
+
+function updateTopImageTheme(isLight) {
+  var image = document.getElementById("main_image");
+  if (!image) {
+    return;
+  }
+
+  var src = image.getAttribute("src");
+  if (!src || !/\.png(\?.*)?$/i.test(src)) {
+    return;
+  }
+
+  var nextSrc = getThemedImageSrc(src, isLight);
+  switchTopImageSmooth(image, nextSrc);
+}
+
 function toggleTheme() {
   var link = document.querySelector("link[id='theme-link']");
   var isLight = link.getAttribute("href") == "../common/css/light-style.css";
@@ -5,9 +51,11 @@ function toggleTheme() {
   if (isLight) {
     link.setAttribute("href", "../common/css/style.css");
     localStorage.setItem("theme", "dark");
+    updateTopImageTheme(false);
   } else {
     link.setAttribute("href", "../common/css/light-style.css");
     localStorage.setItem("theme", "light");
+    updateTopImageTheme(true);
   }
 }
 
@@ -17,7 +65,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (theme == "light") {
     link.setAttribute("href", "../common/css/light-style.css");
+    updateTopImageTheme(true);
   } else {
     link.setAttribute("href", "../common/css/style.css");
+    updateTopImageTheme(false);
   }
 });
